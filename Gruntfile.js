c920039d33de8e60e440eb191514f342b7dd46eb
Anderson Aguiar <revsoft10@gmail.com>
2014-09-13 00:14:46 -0300

    add task to update and commit list

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = function(grunt) {
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        exec: {
+            copy: 'cp  ~/Dropbox/ST3/User/Package\\ Control.sublime-settings .'
+        },
+
+        bump: {
+            options: {
+                // files: ['Package Control.sublime-settings'],
+                commit: true,
+                commitMessage: 'Update list of plugins',
+                commitFiles: ['-a'],
+                push: true,
+                createTag: false,
+                pushTo: 'origin'
+            }
+        }
+    });
+
+    grunt.registerTask('build', ['exec:copy', 'bump']);
+};
